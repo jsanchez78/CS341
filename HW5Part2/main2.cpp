ba758a4a275ce2fb752c9dfad9aa363e08730fdf
Jacob Sanchez <sanchezjacob60@yahoo.com>
2019-09-19 18:22:51 -0500

    sfsdf

--- HW5Part2/main2.cpp
+++ HW5Part2/main2.cpp
@@ -20,9 +20,8 @@
 
 using namespace std;
 
-
 //
-// input(filename):x
+// input(filename):
 //
 // Inputs student data from the file and returns a map of Student objects.
 //
@@ -57,11 +56,11 @@ map<string,Student> input(string filename)
 
     //
     // TODO #1: insert student into map, using name as the key.
-    // Insert using map's overloaded [ ] operator:
-    //
-    // students[???] = ???;
-
-  students[name] = S;
+    // Insert using map's insert() method, which requires the
+    // insert of a (key, value) pair.
+    //Format std::par<type a,type b>(a,b);
+    auto keyvaluepair = std::pair<string, Student>(name,S);///Type of pair
+    students.insert(keyvaluepair);//map.insert(std::pair<string,Student>);
   }
 
   return students;
@@ -81,9 +80,6 @@ int main()
   // 2. Output:
   cout << "** students **" << endl;
 
-
-  //Iterator option but not necessary
-  //map<string,Student>::iterator it = students.begin();
   for(const auto& keyvaluepair : students)
   {
     //
@@ -113,31 +109,34 @@ int main()
     // of students, but in terms of (key, value) pairs.  The lambda expression
     // takes a (key, value) pair as it's argument.  And the find_if function
     // will return an iterator denoting a (key, value) pair.
-    //
+
+
+    //Iterating through std::pair<const string,Student> references
     auto iter = std::find_if(students.begin(), students.end(),
-      [=](std::pair<const string,Student>& x){///
-        if(x.first == name)
-          return true;
-        else
-          return false;
+      [=](std::pair<const string,Student>& map){
+          return (map.first == name);
       }
-      );
-     if (iter == students.end())
+   );
+    //
+    if (iter == students.end())
        cout << "not found" << endl;
      else
-       cout << name << ":" << students[name].ExamAvg() << endl;
+       cout << name << ":" << iter->second.ExamAvg() << endl;
+
 
     //
-    // TODO #4: now lookup using map's overloaded [ ] operator:
+    // TODO #4: now lookup using map's find() method. Much like
+    // std::find_if, find() returns an iterator to a (key, value)
+    // pair.
     //
-    // NOTE: this has the advantage that [key] will return the
-    //  value associated with this key; the (key, value) pairs
-    //  are hidden.  But there's a disadvantage that you'll see
-    //  when you run and test...
+    auto iter2 = students.find(name);
     //
-    Student s = students[name];
-    //
-    cout << s.Name << ":" << s.ExamAvg() << endl;
+     if (iter2 == students.end())
+       cout << "not found" << endl;
+     else
+        cout << name << ":" << iter2->second.ExamAvg() << endl;
+
+
 
     //
     // prompt for next name and repeat:
