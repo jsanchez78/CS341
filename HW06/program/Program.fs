dd667e33dc09962f80c21c25e20e96cc7f71f798
Jacob Sanchez <sanchezjacob60@yahoo.com>
2019-10-06 22:13:29 -0500

    F# HW6 recursive sum functions

---
+++ HW06/program/Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printf "filename> "
+    let filename  = System.Console.ReadLine()
+    let data_array = System.IO.File.ReadAllLines(filename)
+    let data_list = Array.toList data_array
+     //
+     // convert strings to integers:
+     //
+    let values = List.map (fun s -> int s) data_list
+    printfn "%A" values
+     //
+     //Recursive call to legnth function using tail method
+    let rec length list = 
+       match list with
+         | [] -> 0
+         | _ :: tail -> 1 + length tail
+
+    let len = length values
+    printfn "%A" len
+     //Recursive sum list
+    let rec sum list = 
+        match list with
+            | [] -> 0
+            | x :: xs -> x + sum xs
+    let total = sum values
+    printfn "%A" total
+     //
+    0 // return an integer exit code
